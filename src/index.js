9720233d8301a06edcbfcc3595fb53cf6246cd3d
MohmedIkram <53212527+MohmedIkram@users.noreply.github.com>
2021-07-04 14:54:45 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App, BackButton } from "./App";
+import reportWebVitals from "./reportWebVitals";
+import MyAppBar from "./MyAppBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <MyAppBar />
+          <App />
+        </Route>
+        <Route exact path="/about">
+          <MyAppBar />
+          <BackButton />
+          this is about page
+        </Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
