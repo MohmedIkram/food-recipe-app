e8cce1f53df951e89137c7bfed66103217001aff
MohmedIkram <mohmedikram123@gmail.com>
2021-11-11 14:39:38 +0530

    update

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -9,21 +9,11 @@ import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import FastfoodIcon from "@material-ui/icons/Fastfood";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      Your Website {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -57,8 +47,18 @@ export default function SignUp() {
     axios.post(
       `https://url-shortener-server-guvi.herokuapp.com/users/signup`,
       myData
-    );
-    History.push("/");
+    )
+      .then((response) => {
+        // return  response;
+        localStorage.setItem("auth", JSON.stringify(response.data));
+        const token = localStorage.getItem("token");
+        History.push(`/home`);
+      })
+      .catch((error) => {
+        //return  error;
+        History.push(`/addrecipe`);
+      });
+
   };
 
   const [name, setName] = useState("");
@@ -107,8 +107,8 @@ export default function SignUp() {
                 required
                 fullWidth
                 // name="password"
-                label="Password"
-                type="password"
+                label="Recipe"
+                type="text"
                 id="password"
                 autoComplete="current-password"
                 onChange={(e) => setPassword(e.target.value)}
@@ -123,20 +123,10 @@ export default function SignUp() {
             className={classes.submit}
             onClick={handleLogin}
           >
-            Sign Up
+            Add Recipie
           </Button>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <Link to="/" ariant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
         </form>
       </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
     </Container>
   );
 }
